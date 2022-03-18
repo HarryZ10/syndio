81a4832c203bddc11407ced785657cee1f54ecae
Harry <hzhu20@georgefox.edu>
2022-03-17 17:01:59 -0700

    Done

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,13 +2,32 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 
 
+/**
+ * Building the Card component displays the title and text of the data.
+ * @param {*} props - The props passed to the component.
+ * @returns {JSX} - The JSX to be rendered.
+ */
 function CardComponent(props) {
+
+    // destructuring props to get the title and text for readibility
+    const { title, text } = props;
+
+    // bold text that have ¢, $, %, etc.
+    const boldText = text.split(' ').map((word, index) => {
+        if (word.includes('$') || word.includes('¢') || word.includes('%')) {
+            return <span key={index} style={{ fontWeight: 700 }}>{word} </span>
+        } else {
+            return <span key={index}>{word} </span>
+        }
+    });
 
     return (
         <Card style={{ width: '100%', height: '100%', border: 'none', borderBlockColor: 'white' }}>
             <Card.Body>
-                <Card.Title id="card-title">{props.title}</Card.Title>
-                <Card.Text id="card-text">{props.text}</Card.Text>
+                <Card.Title id="card-title">{title}</Card.Title>
+                <Card.Text id="card-text">
+                    {boldText}
+                </Card.Text>
             </Card.Body>
         </Card>
     )
--- src/components/ToggleContent.jsx
+++ src/components/ToggleContent.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
@@ -8,6 +8,11 @@ import Tabs from './Tabs';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+/**
+ * ToggleContent component
+ * @returns {JSX} - The JSX to be rendered.
+ */
 function ToggleContent() {
 
     const { data, loading, error } = useFetch(
@@ -21,7 +26,20 @@ function ToggleContent() {
         }
     );
 
-    
+    // Some Notes:
+
+    // What I would do if I had more time is to use a hook to fetch the data
+    // as shown in the useFetch hook above. Also parsing the data to constants
+    // and then using them in the render function.
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error!</div>;
+    }
+
     return (
         <div>
             <Tabs>
@@ -30,13 +48,13 @@ function ToggleContent() {
                         <Container fluid>
                         <Row>
                             <Col lg>
-                                <Card title="Hello 1" text="11"/>
+                                <Card title="Pay Equity Gap" text="Women earn 96¢ for every $1 earned by comparable men"/>
                             </Col>
                             <Col lg>
-                                <Card title="Hello World" text="22"/>
+                                <Card title="Employees in Comparison" text="Women make up 41% of employees"/>
                             </Col>
                             <Col lg>
-                                <Card title="Hello 3" text="33"/>
+                                <Card title="Budget" text="$235,900 minimum recommended budget to reduce pay equity gap"/>
                             </Col>
                         </Row>
                         </Container>
@@ -48,13 +66,13 @@ function ToggleContent() {
 
                         <Row>
                             <Col lg>
-                                <Card title="Hello 1" text="11"/>
+                                <Card title="Pay Equity Gap" text="Hispanics earn 86¢ for every $1 earned by comparable Whites"/>
                             </Col>
                             <Col lg>
-                                <Card title="Hello 2" text="99"/>
+                                <Card title="Employees in Comparison" text="Hispanics make up 21% of employees"/>
                             </Col>
                             <Col lg>
-                                <Card title="Hello 3" text="33"/>
+                                <Card title="Budget" text="$535,000 minimum recommended budget to reduce pay equity gap"/>
                             </Col>
                         </Row>
                         </Container>
@@ -64,7 +82,6 @@ function ToggleContent() {
             </Tabs>
         </div>
     )
-    
 }
 
 
